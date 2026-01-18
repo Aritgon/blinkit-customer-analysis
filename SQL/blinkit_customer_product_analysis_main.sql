eae3fc7215cf5ba2e35e3bc03a823d6a40ca5bf9
Aritgon <aritgon1947@gmail.com>
2026-01-18 22:42:58 +0530

    sql analysis day 14

--- SQL/blinkit_customer_product_analysis_main.sql
+++ SQL/blinkit_customer_product_analysis_main.sql
@@ -118,10 +118,10 @@ group by a.customer_id, b.customer_name),
 
 	min monetary -> 22.14 & max monetary -> 21686.80
 	for setting up monetary_score, we will follow the following formula,
-		monetary > 8000 then rank 1
-		monetary > 5000 and < 8000, then rank 2
-		monetary > 2000 and < 5000, then rank 3
-		monetary < 1000 and < 2000, then rank 4
+		when monetary > 5000 then 1
+		when monetary between 3001 and 5000 then 2
+		when monetary between 1501 and 3000 then 3
+		when monetary between 801 and 1500 then 4
 		else 5
 */
 
@@ -142,9 +142,9 @@ score_cte as
 		else 5
 	end as r_score,
 	case
-		when frequency between 9 and 7 then 1
-		when frequency between 6 and 4 then 2
-		when frequency between 3 and 2 then 3
+		when frequency >= 7 and frequency <= 9 then 1
+		when frequency >= 4 and frequency <= 6 then 2
+		when frequency = 3 then 3
 		when frequency = 2 then 4
 		else 5
 	end as f_score,
@@ -166,28 +166,254 @@ from cust_details_cte
 
 select
 	-- merging all the scores first.
-
 	r_score,
 	f_score,
 	m_score,
 	
 	(r_score * 100 + f_score * 10 + m_score) as rfm_bins,
-	-- counting customers.
 
-	-- stats per group.
 	round(avg(recency)::decimal, 2) as avg_recency,
-	round(avg(frequency)::decimal, 2) as avg_frequency,
 	round(avg(monetary)::decimal, 2) as avg_monetary,
+	
+	-- counting customers.
 	count(distinct customer_id) as cust_cnt,
-	(select count(distinct customer_id) from blinkit_orders) as total_cust_cnt,
 	
 	-- using whole customer_base to get pct of customers from each segments.
 	round(count(distinct customer_id) * 100.0 / (select count(distinct customer_id) from blinkit_orders), 2) as cust_size_pct
 from score_cte
 group by r_score, f_score, m_score, rfm_bins
-order by avg(monetary) desc;
+order by cust_size_pct desc;
 
 /*
-	Through this RFM analysis, we acknowledged that 
+	Through this RFM analysis, we acknowledged that segment "531" has the highest customer share in blinkit's data with 10.22%
+	of users. But for blinkit this group of customer is at risk because they have an avg recency approx. ~218 days but spends
+	the 
+	
+*/
+
+
+-- Analysis: Next-Product Propensity (Sequencing)
+/*
+	since we can't do product busket analysis in this dataset as the data is heavily normalized
+	(one row = one order id = one product).
+	we are doing next product after a order for every customer,
+	this will help us what is the next product that customers bought after a order. 
+*/
+
+with product_cte as
+(select
+
+	a.customer_id,
+	a.order_date as curr_order_date,
+	d.product_name as curr_product,
+	a.order_total as curr_order_total,
+	
+	lead(a.order_date) over (partition by a.customer_id order by a.order_date) as next_order_date,
+	lead(d.product_name) over (partition by a.customer_id order by a.order_date) as next_product,
+	lead(a.order_total) over (partition by a.customer_id order by a.order_date) as next_order_total,
+	
+	count(*) as sample_size
+from blinkit_orders as a
+join blinkit_order_items as b on b.order_id = a.order_id
+join blinkit_customer_details as c on c.customer_id = a.customer_id
+join blinkit_products as d on d.product_id = b.product_id
+group by a.customer_id, a.order_date, d.product_name, a.order_total),
+
+-- after that concatenate or count product name wise order size.
+sequential_cte as
+(select
+	curr_product,
+	next_product,
+	count(*) as product_pair_size,
+	round(avg(curr_order_total + next_order_total)::decimal, 2) as avg_paired_order_total
+from product_cte
+where next_product is not null
+group by curr_product, next_product
+having count(*) > 1 -- filtering out one random order pair.
+order by product_pair_size desc)
+
+/*
+	From this sequencing analysis, we acknowledged that baby wipes and vitamins was the first most bought pair.
+	After buying 'baby wipes' customers generally bought 'vitamins' the most. This pair has a size of 9. Meaning, it was bought
+	9 times, averaging rupees 5716.29 of revenue.
+
+	Followed by 'dish soap' + 'potatoes' which was also bought 9 times averaging 4314.02 rupees of revenue.
+
+	After that, 'dish soap' + 'cough syrup' which was bough 8 times in pair averaging 4262.11 rupees of revenue
+	in the third position.
+*/
+
+-- From here Moving onto which product order made more revenue and were ordered the most. 
+select
+	s1.curr_product as item_x,
+	s1.next_product as item_y,
+
+	-- stats.
+	s1.product_pair_size as x_to_y_count,
+	s1.avg_paired_order_total as x_to_y_avg_revenue,
+
+	s2.curr_product as item_x_reverse,
+	s2.next_product as item_y_reverse,
+	
+	-- stats for the next two products bought together.
+	s2.product_pair_size as y_to_x_count,
+	s2.avg_paired_order_total as y_to_x_avg_revenue,
+
+	-- avg gap between first pairing and second pairing.
+	(s1.avg_paired_order_total - s2.avg_paired_order_total) as avg_revenue_gap
+from sequential_cte as s1
+join sequential_cte as s2 on s1.curr_product = s2.next_product -- matching on the items that is first on first pair and second in second pair.
+	and s1.next_product = s2.curr_product -- matching on items that is second in first pair and first in second pair.
+where s1.curr_product > s1.next_product -- filtering out product pairs that 
+order by avg_revenue_gap desc;
+
+/*
+	this analysis is showing order pairs that made how much revenue than the same product pairs
+	but in reverse orders made how much revenue.
+*/
+
+-- Analysis: Next-category Propensity (Sequencing)
+with category_cte as
+(select
+	a.customer_id,
+
+	d.category as curr_category,
+	a.order_total as curr_order_total,
+	
+	a.order_date as curr_order_date,
+	lead(d.category) over (partition by a.customer_id order by a.order_date) as next_category,
+	lead(a.order_total) over (partition by a.customer_id order by a.order_date) as next_order_total,
+	lead(a.order_date) over (partition by a.customer_id order by a.order_date) as next_order_date
+	
+from blinkit_orders as a
+join blinkit_order_items as b on b.order_id = a.order_id
+join blinkit_customer_details as c on c.customer_id = a.customer_id
+join blinkit_products as d on d.product_id = b.product_id)
+
+select
+	curr_category,
+	next_category,
+
+	count(*) as pair_ordering_size,
+	round(avg(curr_order_total + next_order_total)::decimal, 2) as avg_revenue_per_pair
+from category_cte
+where next_category is not null
+group by 1, 2
+having count(*) > 1
+order by 3 desc;
+
+-- Next up, we are going to do correlation analysis between delivery timing and customer feedback rating per customer.
+
+/*
+	
+*/
+
+with cust_cte as
+(select
+
+	b.order_id,
+	a.customer_id,
+	b.gap_between_promised_actual_time,
+	a.feedback_category,
+	a.sentiment,
+	a.rating
+	
+from blinkit_customer_feedback as a
+left join blinkit_orders as b on b.order_id = a.order_id) 
+/*
+	using left join to get every order each customers made from the left table.
+	
+	Joining on the order_id for both the tables because only some customers leave a feedback after their order.
+	For example, there might be 10 orders for a customer where they only submitted a feedback for only one order.
+	THe count() on the next query will every row for every order. Now to count order numbers , this is okay.
+	but when we need to use count with case function this will wrongly output the result because every row will be counted
+	based on the case function condition.
+*/
+
+select
+	case
+		when gap_between_promised_actual_time between -5 and -1 then 'slightly late delivery'
+		when gap_between_promised_actual_time between -10 and -6 then 'moderate late delivery'
+		when gap_between_promised_actual_time between -15 and -11 then 'late delivery'
+		when gap_between_promised_actual_time between -20 and -16 then 'very late delivery'
+		when gap_between_promised_actual_time between -25 and -21 then 'concerningly late delivery'
+		when gap_between_promised_actual_time between -30 and -26 then 'customer might churn if delivery becomes this late'
+
+		when gap_between_promised_actual_time between 0 and 5 then 'slightly fast delivery'
+		when gap_between_promised_actual_time between 6 and 10 then 'moderate late delivery'
+		when gap_between_promised_actual_time between 11 and 15 then 'late delivery'
+		when gap_between_promised_actual_time between 16 and 20 then 'very late delivery'
+		when gap_between_promised_actual_time between 21 and 25 then 'concerningly late delivery'
+	else 'other timing'
+	end as delivery_timing_range,
+
+	-- stats.
+	round(avg(gap_between_promised_actual_time)::decimal, 2) as avg_gap_between_promised_and_actual_time,
+	count(order_id) as order_cnt,
+	
+	-- getting negative sentiments feedback count and pct from all feedbacks recorded for each delivery type.
+	count(case when sentiment = 'negative' then 1 end) as negative_fdbk_cnt,
+
+	count(case when sentiment = 'negative' and feedback_category = 'delivery' then 1 end) as negative_fdbk_regarding_delivery_cnt,
+	-- round(count(case when sentiment = 'negative' and feedback_category = 'delivery' then 1 end) * 100.0 / 
+	-- count(order_id), 2) as negative_fdbk_pct
+
+	round(count(case when sentiment = 'negative' and feedback_category = 'delivery' then 1 end) * 100.0 / 
+	count(case when sentiment = 'negative' then 1 end), 2) as negative_fdbk_for_delivery_pct,
+	
+	-- count(case when sentiment = 'positive' then 1 end) as positive_fdbk_cnt,
+	-- count(case when sentiment = 'neutral' then 1 end) as neutral_fdbk_cnt
+
+	round(count(case when sentiment = 'negative' and feedback_category = 'delivery' then 1 end) * 100.0 
+	/ sum(count(*)) over (), 2) as delivery_negative_reviews_pct,
+
+	-- getting order contribution pct of each segments from total orders.
+	round(count(order_id) * 100.0 / sum(count(order_id)) over (), 2) as order_contribution_pct
+	
+from cust_cte
+group by delivery_timing_range
+order by delivery_negative_reviews_pct desc;
+
+/*
+	according to analysis, surprisingly, the positive category which is 'slightly fast delivery' got the most negative reviews 
+	for delivery being late than promised delivery time among all orders. 
+	
+	This category had an average time (promised_delivery_time - actual_delivery_time) 2.47 in minutes.
+	it signifies that customers aren't satisfied with blinkit delivery timing even 
+	if the average between promised and actual timing is 2.47 minutes.
+
+	After that, we have 'slightly late delivery' at the second position where the most negative reviews 
+	for delivery being late than promised delivery time among all orders.
+	This category had an average timing (promised_delivery_time - actual_delivery_time) as -3.02 in minutes.
+
+	both of this segments have the most combined negative review pct among all orders (~6%) which maintained
+	a close delivery timing while sometimes being 2.47 minutes fast (avg) and 3.02 minutes late than promised delivery time.
 
 */
+
+
+-- analysing which company is making the most revenue/loss monthly wise.
+
+select
+	p.brand,
+	p.category,
+	p.product_name,
+	
+	-- product stats.
+	round(sum(p.price)::decimal, 2) as total_cost_price,
+	round(sum(o.order_total)::decimal, 2) as total_order_value,
+
+	round(sum(o.order_total)::decimal - sum(p.price)::decimal, 2) as revenue_gap
+from blinkit_orders as o
+join blinkit_order_items as oi on oi.order_id = o.order_id
+join blinkit_products as p on p.product_id = oi.product_id
+group by p.brand, p.category, p.product_name
+order by p.brand;
+
+
+
+
+
+
+
+
